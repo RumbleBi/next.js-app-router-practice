87e79fffcb9addaaa6f4a17bbb8c08976233353e
RumbleBi <olzlel2000@gmail.com>
2024-11-16 17:31:56 +0900

    practice

--- src/app/(with-searchbar)/search/page.tsx
+++ src/app/(with-searchbar)/search/page.tsx
@@ -1,19 +1,14 @@
 import BookItem from "@/components/book-item";
+import BookListSkeleton from "@/components/skeleton/book-list-skeletion";
 import { BookData } from "@/types";
 import { delay } from "@/util/delay";
+import { Suspense } from "react";
 
-export default async function Page({
-  searchParams,
-}: {
-  searchParams: {
-    q?: string;
-  };
-}) {
+async function SearchResult({ q }: { q: string }) {
   await delay(1500);
-  const response = await fetch(
-    `${process.env.NEXT_PUBLIC_API_SERVER_URL}/book/search?q=${searchParams.q}`,
-    { cache: "force-cache" }
-  );
+  const response = await fetch(`${process.env.NEXT_PUBLIC_API_SERVER_URL}/book/search?q=${q}`, {
+    cache: "force-cache",
+  });
   if (!response.ok) {
     return <div>오류가 발생했습니다...</div>;
   }
@@ -26,5 +21,19 @@ export default async function Page({
         <BookItem key={book.id} {...book} />
       ))}
     </div>
+  );
+}
+
+export default function Page({
+  searchParams,
+}: {
+  searchParams: {
+    q?: string;
+  };
+}) {
+  return (
+    <Suspense key={searchParams.q || ""} fallback={<BookListSkeleton count={3} />}>
+      <SearchResult q={searchParams.q || ""} />
+    </Suspense>
   );
 }
